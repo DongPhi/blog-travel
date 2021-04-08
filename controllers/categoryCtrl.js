34f3bffa880906b0de6c3b1a46962c56b97b42ca
DongPhi <dongphivnn@gmail.com>
2021-04-08 22:57:23 +0700

    Update categoryCtrl.js

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -3,24 +3,32 @@ const Category = require('../models/categoryModel');
 const categoryCtrl = {
     getCategories: async(req, res) =>{
         try {
-            const categories = await Category.find();
-            res.json(categories);
+            const categories = await Category.find()
+            res.json(categories)
         } catch (err) {
-            return res.status(500).json({msg: err.message});
+            return res.status(500).json({msg: err.message})
         }
     },
-    createCategory: async(req, res) =>{
+    createCategory: async (req, res) =>{
         try {
+<<<<<<< Updated upstream
             //if user have role = 1 --> admin
             //only admin can create, delete and update category
             const {name} = req.body;
             const category = await Category.findOne({name});
             if(category) return res.status(400).json({msg:"This category alrealy exists."});
             const newCategory = new Category({name});
+=======
+            // if user have role = 1 ---> admin
+            // only admin can create , delete and update category
+            const {name} = req.body;
+            const category = await Category.findOne({name})
+            if(category) return res.status(400).json({msg: "This category already exists."})
+>>>>>>> Stashed changes
 
-            await newCategory.save();
-            res.json({msg: "Create a category"});
+            const newCategory = new Category({name})
 
+<<<<<<< Updated upstream
         } catch (err) {
             return res.status(500).json({msg: err.message});
         }
@@ -39,10 +47,14 @@ const categoryCtrl = {
             await Category.findOneAndUpdate({_id: req.params.id}, {name});
 
             res.json({msg: " Update a category"});
+=======
+            await newCategory.save()
+            res.json({msg: "Created a category"})
+>>>>>>> Stashed changes
         } catch (err) {
-            return res.status(500).json({msg: err.message});
+            return res.status(500).json({msg: err.message})
         }
-    }
+    },
 }
 
 module.exports = categoryCtrl
